122ddd4d7047020ff562b45b426d6502a84bca1f
Sujit Maharjan <Sujit.Maharjan@logpoint.com>
2020-07-12 08:33:04 +0545

    code that converts date and setup.py file to create python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+long_description = ""
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datebs", # Replace with your own username
+    version="0.0.1",
+    author="Sujit Maharjan",
+    author_email="shubhajeet.per@gmail.com",
+    description="convert date from BS to AD and vice versa",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shubhajeet/datebspy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
